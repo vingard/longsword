39b29926897d564148cf87c1f3081cd91def4f98
Jake <jaketg10@gmail.com>
2021-02-03 02:16:29 +0000

    vital fix

--- lua/weapons/ls_base.lua
+++ lua/weapons/ls_base.lua
@@ -2,13 +2,13 @@
 
 AddCSLuaFile()
 
-if not ConVarExists("longsword_debug") then
+if CLIENT and not ConVarExists("longsword_debug") then
 	CreateClientConVar("longsword_debug", "0", false, false)
 
 	surface.CreateFont("lsDebug", {
-		font = "Arial",
+		font = "Consolas",
 		size = 18,
-		weight = 800,
+		weight = 1000,
 		antialias = true,
 		shadow = true
 	})
